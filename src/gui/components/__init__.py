4f32805ce2b46aefa34ef44c1397a3df5cf12a4f
Budgulick <budgulick@mrtheguru.com>
2025-08-05 22:19:39 -0500

    Add Output Panel component import to components package

--- src/gui/components/__init__.py
+++ src/gui/components/__init__.py
@@ -6,12 +6,15 @@ Contains all reusable GUI components
 # Phase 1.3 Components
 from .url_manager import URLManager, URLItem
 
+# Phase 1.4 Components  
+from .output_panel import OutputPanel
+
 # Future components will be imported here as they are created
-# Phase 1.4: output_panel
 # Phase 1.5: progress_panel, control_panel
 # Phase 1.6: status_log
 
 __all__ = [
     'URLManager',
-    'URLItem'
+    'URLItem',
+    'OutputPanel'
 ]
