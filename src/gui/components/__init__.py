2403c47a50265c79babd82e099b784cf7b38440a
Budgulick <budgulick@mrtheguru.com>
2025-08-05 22:07:17 -0500

    Add URL Manager component import to components package

--- src/gui/components/__init__.py
+++ src/gui/components/__init__.py
@@ -3,8 +3,15 @@ GUI Components Package
 Contains all reusable GUI components
 """
 
-# Components will be imported here as they are created
-# Phase 1.3: url_manager
+# Phase 1.3 Components
+from .url_manager import URLManager, URLItem
+
+# Future components will be imported here as they are created
 # Phase 1.4: output_panel
 # Phase 1.5: progress_panel, control_panel
 # Phase 1.6: status_log
+
+__all__ = [
+    'URLManager',
+    'URLItem'
+]
